25c9e4663aeb8731ee3bdea7f708b1b0710578e6
ElenaLukashevych <elukashevych@gmail.com>
2022-08-05 15:38:42 +0300

    add

---
+++ README.md
@@ -0,0 +1,11 @@
+list
+https://monosnap.com/file/xUEkef7wskpVZKgIwTWCb1CPNCkfDm
+
+get
+https://monosnap.com/file/gFtFi80BNmaFEudqAJAez7GCHCjlLH
+
+add
+https://monosnap.com/file/XAZU6pebJpjLNT0zpOWgHpgDQMzAJX
+
+remove
+https://monosnap.com/file/HC6CiFW6CMLVbpMVKdpNFvqj5sT0hR
---
+++ contacts.js
@@ -0,0 +1,69 @@
+const fs = require('fs').promises;
+const path = require('path');
+const {v4} = require('uuid');
+
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+async function listContacts() {
+try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+async function getContactById(contactId) {
+ try {
+     const contacts = await listContacts();
+     const result = contacts.find(contact => contact.id === contactId);
+     if (!result) {
+         return null
+     }
+     return result;
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+async function removeContact(contactId) {
+ try {
+     const contacts = await listContacts();
+     const idx = contacts.findIndex(contact => contact.id === contactId);
+     if (idx === -1) {
+         return null;
+     };
+     const [removedContact] = contacts.splice(idx, 1);
+     await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+     return removedContact;
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+async function addContact(name, email, phone) {
+ try {
+     const contacts = await listContacts();
+     const newContact = {
+         id: v4(),
+         name,
+         email,
+         phone,
+     };
+     contacts.push(newContact);
+     await fs.writeFile(contactsPath, JSON.stringify(contacts));
+     return newContact;
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
